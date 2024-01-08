79518b2f51724ff920b9283fc399c0b8a0d6a675
Concorth <112446723+Concorth@users.noreply.github.com>
2024-01-08 13:22:29 +1000

    Update

--- Squid Audio Downloader.bat
+++ Squid Audio Downloader.bat
@@ -1,24 +1,24 @@
 @echo off
 setlocal enabledelayedexpansion
 color d
-echo "   _____  ____  _    _ _____ _____         _____  _      
-echo "  / ____|/ __ \| |  | |_   _|  __ \       |  __ \| |     
-echo " | (___ | |  | | |  | | | | | |  | |______| |  | | |     
-echo "  \___ \| |  | | |  | | | | | |  | |______| |  | | |     
-echo "  ____) | |__| | |__| |_| |_| |__| |      | |__| | |____ 
-echo " |_____/ \___\_\\____/|_____|_____/       |_____/|______|
-echo "          _    _ _____ _____ ____  
-echo "     /\  | |  | |  __ \_   _/ __ \ 
-echo "    /  \ | |  | | |  | || || |  | |
-echo "   / /\ \| |  | | |  | || || |  | |
-echo "  / ____ \ |__| | |__| || || |__| |
-echo " /_/    \_\____/|_____/_____\____/ 
+echo "   _____  ____  _    _ _____ _____
+echo "  / ____|/ __ \| |  | |_   _|  __ \
+echo " | (___ | |  | | |  | | | | | |  | |
+echo "  \___ \| |  | | |  | | | | | |  | |   
+echo "  ____) | |__| | |__| |_| |_| |__| |
+echo " |_____/ \___\_\\____/|_____|_____/ 
+echo "          _    _ _____ _____ ____         _____  _      
+echo "     /\  | |  | |  __ \_   _/ __ \       |  __ \| |     
+echo "    /  \ | |  | | |  | || || |  | |______| |  | | |     
+echo "   / /\ \| |  | | |  | || || |  | |______| |  | | |     
+echo "  / ____ \ |__| | |__| || || |__| |      | |__| | |____ 
+echo " /_/    \_\____/|_____/_____\____/       |_____/|______|                                                      
 echo "
 
 :loop
 color d
 REM Get user input for video link
-set /p "video_link=[AUDIO ONLY DOWNLOAD] Paste YouTube link: "
+set /p "video_link=Paste YouTube link: "
 
 REM Download run download command
 yt-dlp.exe -x --audio-format wav "%video_link%"
